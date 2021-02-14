539222f754ab0d0a3ddd3aabef63978b33ced135
Scott Herren <me@stwoh.com>
2021-02-14 10:29:56 -0600

    fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ module.exports = {
             },
             {
               label: 'Subgraphs',
-              to: 'docs/subgraphs',
+              to: '/subgraphs',
             },
           ],
         },

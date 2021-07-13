9c62fec252ebadc3def99abcb7a30c0e94215c28
amosgoh <gohjiashengamos@gmail.com>
2021-07-13 16:54:42 +0800

    created FAQ section with 10 sub-sections

--- sidebars.js
+++ sidebars.js
@@ -56,6 +56,15 @@ module.exports = {
     UberHaus: ["users/uberhaus-governance", "users/cco", "users/uber-actions"],
     FAQ: [
       "users/proposals-faq",
+      "users/bank-faq",
+      "users/members-faq",
+      "users/settings-faq",
+      "users/apps-faq",
+      "users/allies-faq",
+      "users/profile-faq",
+      "users/uberhaus-faq",
+      "users/minion-faq",
+      "users/other-networks-faq",
     ],
   },
    
